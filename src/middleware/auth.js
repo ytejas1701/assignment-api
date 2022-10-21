7e919e90f4c6c96032a39ef35a5300a873c42acb
Tejas Yadav <ytejas1701@kgpian.iitkgp.ac.in>
2022-10-21 11:59:12 +0530

    small change

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next)=>{
     try{
         const token = req.header('Authorization').replace('Bearer ', '');
 
-        const decoded = jwt.verify(token, 'thisisasecret');
+        const decoded = jwt.verify(token, process.env.JWT_PASSKEY);
 
         const user = await User.findById(decoded._id);
 
